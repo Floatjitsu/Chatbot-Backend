daf5bdbd167c59902449025fb3619c341c88c433
Timur Celik <timur.celik@gmx.de>
2020-05-12 09:47:22 +0200

    firebase test

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,22 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccount.json');
+
+admin.initializeApp({
+  	credential: admin.credential.cert(serviceAccount),
+  	databaseURL: "https://chatbot-database-dc942.firebaseio.com"
+});
+
+const db = admin.database();
+const ref = db.ref('auftraege');
+
 exports.handler = async event => {
+	ref.on('value', (snapshot) => {
+		console.log(snapshot.val());
+	}, (error) => {
+		console.log('The read failed: ' + error.code);
+	});
 	const subject = event.queryStringParameters.name || 'World';
+	console.log('Hello World!');
 	return {
 		statusCode: 200,
 		body: `Hello ${subject}!`
